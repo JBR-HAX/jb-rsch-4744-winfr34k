63ebf0fcb10cd3adff827882581bb8e898609600
Thomas Keppler <winfr34k@gmail.com>
2024-02-27 19:18:22 +0100

    Model a robot's walk using cooredinates and directions
    Using the two constructs of the README, we can model each direction
    depicted in the examples.
    
    These classes are "pure data types", so we can share them in the
    controller layer later.

---
+++ src/main/java/org/jetbrains/assignment/Coordinate.java
@@ -0,0 +1,11 @@
+package org.jetbrains.assignment;
+
+/**
+ * Represents the coordinate in the system present in README.md.
+ *
+ * NOTE: This system seems to be flipped (Y vs. X).
+ *
+ * @param x The vertical axis. Positive values go North, negative values go South.
+ * @param y The horizontal axis. Positive values go East, negative values go West.
+ */
+public record Coordinate(int x, int y) { }
---
+++ src/main/java/org/jetbrains/assignment/Direction.java
@@ -0,0 +1,26 @@
+package org.jetbrains.assignment;
+
+/**
+ * Walking direction of a robot.
+ */
+public enum Direction {
+    /**
+     * Positive movement on the X (vertical) axis.
+     */
+    NORTH,
+
+    /**
+     * Positive movement on the Y (horizontal) axis.
+     */
+    EAST,
+
+    /**
+     * Negative movement on the X (vertical) axis.
+     */
+    SOUTH,
+
+    /**
+     * Negative movement on the Y (horizontal) axis.
+     */
+    WEST
+}
