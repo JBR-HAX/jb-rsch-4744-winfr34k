d53a9f60c9cfe4c72d76124cf57bda4b52f1dcb2
Thomas Keppler <winfr34k@gmail.com>
2024-02-27 19:24:26 +0100

    Model a robot with movements
    Using `synchronized { }` is not great, but we will need to make sure
    that we can't just clobber previous results when a multi-threaded
    construct (like the controller in our web application) modifies the
    robot's posiiton concurrently.

---
+++ src/main/java/org/jetbrains/assignment/Robot.java
@@ -0,0 +1,34 @@
+package org.jetbrains.assignment;
+
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+@Component
+public class Robot {
+    private static final Object LOCK = new Object();
+
+    private Coordinate position = new Coordinate(0, 0);
+
+    public Coordinate getPosition() {
+        return new Coordinate(position.x(), position.y());
+    }
+
+    public Coordinate move(final Direction direction, int steps) {
+        Assert.isTrue(steps > 0, "Steps need to be positive!");
+
+        // FIXME: This is a primitive solution, maybe come up with a more performant idea later?
+        //        What about lock free data structures?
+        synchronized(LOCK) {
+            final var newPosition = switch (direction) {
+                case NORTH -> new Coordinate(position.x() + steps, position.y());
+                case EAST -> new Coordinate(position.x(), position.y() + steps);
+                case SOUTH -> new Coordinate(position.x() - steps, position.y());
+                case WEST -> new Coordinate(position.x(), position.y() - steps);
+            };
+
+            this.position = newPosition;
+
+            return newPosition;
+        }
+    }
+}
---
+++ src/test/java/org/jetbrains/assignment/RobotTest.java
@@ -0,0 +1,73 @@
+package org.jetbrains.assignment;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RobotTest {
+
+    @Test
+    public void initialPosition() {
+        final var robot = new Robot();
+
+        final var currentPosition = robot.getPosition();
+
+        assertThat(currentPosition).isEqualTo(new Coordinate(0, 0));
+    }
+
+    @Test
+    public void cannotMoveNegativeSteps() {
+        final var robot = new Robot();
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> robot.move(Direction.NORTH, -1));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> robot.move(Direction.EAST, -1));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> robot.move(Direction.SOUTH, -1));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> robot.move(Direction.WEST, -1));
+    }
+
+    @Test
+    public void movingNorth() {
+        final var robot = new Robot();
+
+        final var newPosition = robot.move(Direction.NORTH, 1);
+
+        assertThat(newPosition).isEqualTo(new Coordinate(1, 0));
+    }
+
+    @Test
+    public void movingEast() {
+        final var robot = new Robot();
+
+        final var newPosition = robot.move(Direction.EAST, 1);
+
+        assertThat(newPosition).isEqualTo(new Coordinate(0, 1));
+    }
+
+    @Test
+    public void movingSouth() {
+        final var robot = new Robot();
+
+        final var newPosition = robot.move(Direction.SOUTH, 1);
+
+        assertThat(newPosition).isEqualTo(new Coordinate(-1, 0));
+    }
+
+    @Test
+    public void movingWest() {
+        final var robot = new Robot();
+
+        final var newPosition = robot.move(Direction.WEST, 1);
+
+        assertThat(newPosition).isEqualTo(new Coordinate(0, -1));
+    }
+
+    @Test
+    public void honorsStepSize() {
+        final var robot = new Robot();
+
+        final var newPosition = robot.move(Direction.NORTH, 3);
+
+        assertThat(newPosition).isEqualTo(new Coordinate(3, 0));
+    }
+}
